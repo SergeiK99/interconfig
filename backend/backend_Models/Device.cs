231a0725238b69a6db6cd3f131bb2a2b5014392d
Сергей Колесников <skx2013@mail.ru>
2025-03-23 16:18:31 +0700

    Созданы сущности/классы

---
+++ backend/backend_Models/Device.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace backend_Models
+{
+    public class Device
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Url { get; set; }
+        [Required]
+        public double Performance { get; set; }
+        [Range(1, int.MaxValue)]
+        public int Price { get; set; }
+        public int VentilationTypeId { get; set; }
+        [ForeignKey("VentilationTypeId")]
+        public virtual required VentilationType VentilationType { get; set; }
+    }
+}
---
+++ backend/backend_Models/VentilationType.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace backend_Models
+{
+    public class VentilationType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
